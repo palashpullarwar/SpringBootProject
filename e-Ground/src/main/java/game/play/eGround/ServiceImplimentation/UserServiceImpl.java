c690d9b5b2cca80173308df17d7c8ef963867f44
name <palashp@cdac.in>
2023-01-27 10:07:11 +0530

    added custom exception handling

--- e-Ground/src/main/java/game/play/eGround/ServiceImplimentation/UserServiceImpl.java
+++ e-Ground/src/main/java/game/play/eGround/ServiceImplimentation/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package game.play.eGround.ServiceImplimentation;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import game.play.eGround.ServiceImplimentation.services.UserService;
 import game.play.eGround.ServiceImplimentation.services.userDao.UserRepository;
 import game.play.eGround.entity.User;
+import game.play.eGround.exceptions.AlreadyExistsException;
 import game.play.eGround.exceptions.ResourceNotFoundException;
 
 @Service
@@ -21,22 +24,34 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public User getUserById(int userId) {
-			try {
+	public User getUserById(int userId){
+		
+//			Optional<User> user = userRepo.findById(userId);
+//			System.out.println(user.get());
+//				 if(user.isEmpty()) {
+//					 throw new ResourceNotFoundException("User not found");
+//				 }
+//				 return user.get();
 				 
-				 Optional<User> user = userRepo.findById(userId);
-				 if(user.get().equals(null)) {
-					 throw new ResourceNotFoundException();
-				 }
-				 return user.get();
-			} catch (Exception e) {
-				throw new ResourceNotFoundException(e);
-			}
-			
+				 try {
+						Optional<User> user = userRepo.findById(userId);
+						return user.get();
+					} catch (Exception e) {
+						throw new ResourceNotFoundException(e);
+					}
+				
 	}
 
 	@Override
 	public User addUser(User user) {
+		List<User> users = userRepo.findAll();
+		
+		for(User u : users) {
+			if(u.getEmail().equals(user.getEmail())) {
+				throw new AlreadyExistsException("User already exists");
+			}
+		}
+		
 		userRepo.save(user);
 		return user;
 	}
--- e-Ground/src/main/java/game/play/eGround/globalExceptions/GlobalException.java
+++ e-Ground/src/main/java/game/play/eGround/globalExceptions/GlobalException.java
@@ -1,10 +1,12 @@
 package game.play.eGround.globalExceptions;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import game.play.eGround.errorResponse.ErrorResponse;
 import game.play.eGround.exceptions.AlreadyExistsException;
 import game.play.eGround.exceptions.ResourceNotFoundException;
@@ -12,24 +14,38 @@ import game.play.eGround.exceptions.ResourceNotFoundException;
 @ControllerAdvice
 public class GlobalException {
 
-	public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex){
-		
+	@ExceptionHandler(value = ResourceNotFoundException.class)
+	public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+
 		ex.printStackTrace();
 		List<String> details = new ArrayList<>();
 		details.add(ex.getMessage());
 		ErrorResponse error = new ErrorResponse("User does not exists", details);
-		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
 	}
-	
-public ResponseEntity<Object> handleAlreadyExistsException(AlreadyExistsException ex){
-		
+
+	@ExceptionHandler(value = AlreadyExistsException.class)
+	public ResponseEntity<Object> handleAlreadyExistsException(AlreadyExistsException ex) {
+
 		ex.printStackTrace();
 		List<String> details = new ArrayList<>();
 		details.add(ex.getMessage());
 		ErrorResponse error = new ErrorResponse("User already exists", details);
-		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
 	}
-	
+
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<Object> handleException(Exception ex) {
+
+		ex.printStackTrace();
+
+		List<String> details = new ArrayList<>();
+		details.add(ex.getLocalizedMessage());
+		ErrorResponse error = new ErrorResponse("Unknown Exception", details);
+		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+
 }
